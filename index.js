86cbf0e2a9a90b27815928e06d173937700e0ec1
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-02-05 15:08:10 +1100

    Create product detail page for each product.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require("path");
 const mongoose = require("mongoose");
 
 const Product = require("./models/product");
-const { log } = require("console");
 
 mongoose
   .connect("mongodb://127.0.0.1/farmStand", {
@@ -25,6 +24,12 @@ app.set("view engine", "ejs");
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   res.render("products/index", { products });
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/show", { product });
 });
 
 app.listen(3000, () => {
